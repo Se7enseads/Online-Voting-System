621fd1a1ddb46a328837b99c3eb243c58b9f7cd1
sam kariuki <samuel.kariuki1@student.moringaschool.com>
2023-10-01 22:32:08 +0300

    images added

--- frontend/src/componets/HomePage.jsx
+++ frontend/src/componets/HomePage.jsx
@@ -2,42 +2,38 @@ import React from 'react';
 
 function HomePage() {
   return (
-    <section className= "container-fluid">
-     
-        <div className="container card-body">
-          <div className="row">
-            <div className="col-md-4">
-              <h1 className="title is-italic text-success display-1">
-                Vote App
-              </h1>
-              <h2 className="subtitle text-dark h4">Login and start voting</h2>
-              <p className="font-weight-bold text-dark text-left">
-                Created a simple college election voting app using Flask and
-                Bulma, where students can register themselves. Once registered,
-                students can log in and cast their votes.
-              </p>
-              <br />
-              <p className="font-weight-bold text-dark text-left">
-                Candidate info tab gives you information about the candidates
-                standing for the elections.
-              </p>
-              <br />
-              <p className="font-weight-bold text-dark text-left">
-                A new candidate can be registered only by the Admin. Admin can
-                log in and fill a form to register a new candidate for the
-                election.
-              </p>
-            </div>
-            <div className="col-md-8">
-              <img
-                src="frontend/vote2.jpg"
-                alt="Vote App"
-                className="img-fluid"
-              />
-            </div>
+    <section className="container-fluid bg-light">
+      <div className="card-body container mt-5">
+        <div className="row">
+          <div className="col-md-4">
+            <h1 className="title is-italic text-success display-1">Vote App</h1>
+            <h2 className="subtitle text-dark h4">Login and start voting</h2>
+            <p className="font-weight-bold text-dark text-left">
+              Created a simple college election voting app using Flask and
+              Bulma, where students can register themselves. Once registered,
+              students can log in and cast their votes.
+            </p>
+            <br />
+            <p className="font-weight-bold text-dark text-left">
+              Candidate info tab gives you information about the candidates
+              standing for the elections.
+            </p>
+            <br />
+            <p className="font-weight-bold text-dark text-left">
+              A new candidate can be registered only by the Admin. Admin can log
+              in and fill a form to register a new candidate for the election.
+            </p>
+          </div>
+          <div className="col-md-8">
+            <img
+              src="images/vote1.jpg"
+              alt="Vote App"
+              className="img-fluid "
+              style={{ maxHeight: '400px', width: '100%', objectFit: 'cover' }}
+            />
           </div>
         </div>
-    
+      </div>
     </section>
   );
 }
