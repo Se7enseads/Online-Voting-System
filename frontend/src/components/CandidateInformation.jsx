9bb6d560a19dc9fa05509f89f95d70b091ac5d86
sam kariuki <samuel.kariuki1@student.moringaschool.com>
2023-10-04 22:50:29 +0300

    candidate update

--- frontend/src/components/CandidateInformation.jsx
+++ frontend/src/components/CandidateInformation.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 
+
 function CandidateInformation() {
   const [prez, setPrez] = useState([]);
   const [vice, setVice] = useState([]);
@@ -19,10 +20,10 @@ function CandidateInformation() {
   }, []);
 
   return (
-    <div className="card-body hero has-background-white-ter is-fullheight-with-navbar">
+    <div className="hero is-fullheight-with-navbar">
       <div className="hero-body">
         <div className="container">
-          <div className="columns">
+          <div className="columns is-centered">
             <div className="column is-12">
               <h1 className="title has-text-success">Candidate Information</h1>
               <h2 className="subtitle is-5 has-text-dark has-text-centered has-text-weight-bold">
@@ -32,7 +33,7 @@ function CandidateInformation() {
               </h2>
               <br />
               <br />
-              <div className="column id-9">
+              <div className="column is-9">
                 <h1 className="title has-text-success">
                   Presidential Candidates
                 </h1>
@@ -41,32 +42,36 @@ function CandidateInformation() {
                   {prez.map((prezCandidate) => (
                     <div className="tile is-parent" key={prezCandidate.id}>
                       {prezCandidate.last_name !== 'Vote' && (
-                        <article className="tile is-child box notification is-white">
-                          <h1 className="title is-5 has-text-dark">
-                            {prezCandidate.first_name} {prezCandidate.last_name}
-                          </h1>
-                          <div className="card-content">
-                            <p className="subtitle is-3 has-text-dark">
-                              {prezCandidate.roll_num}
-                            </p>
-                            <p className="title is-5 has-text-dark is-italic">
-                              "{prezCandidate.agenda}"
-                            </p>
-                            <p className="subtitle has-text-dark has-text-weight-semibold">
-                              {prezCandidate.batch}
-                            </p>
-                            <p className="subtitle has-text-dark has-text-weight-semibold">
-                              {prezCandidate.course}, {prezCandidate.department}
-                            </p>
+                        <div className="card">
+                          <div className="card-content has-text-centered">
+                            <div className="circle-image">
+                              <img
+                                className="is-rounded"
+                                src="public/images/OTIS.jpeg"
+                                alt="Candidate Image"
+                              />
+                            </div>
+                            <div className="card-text">
+                              <h1 className="title is-5 has-text-dark">
+                                {prezCandidate.first_name}{' '}
+                                {prezCandidate.last_name}
+                              </h1>
+                              <p className="subtitle is-3 has-text-dark">
+                                {prezCandidate.roll_num}
+                              </p>
+                              <p className="title is-5 has-text-dark is-italic">
+                                "{prezCandidate.agenda}"
+                              </p>
+                              <p className="subtitle has-text-dark has-text-weight-semibold">
+                                {prezCandidate.batch}
+                              </p>
+                              <p className="subtitle has-text-dark has-text-weight-semibold">
+                                {prezCandidate.course},{' '}
+                                {prezCandidate.department}
+                              </p>
+                            </div>
                           </div>
-                          <figure className="image">
-                            <img
-                              className="is-rounded"
-                              src="public/images/OTIS.jpeg"
-                              alt="Candidate Image"
-                            />
-                          </figure>
-                        </article>
+                        </div>
                       )}
                     </div>
                   ))}
@@ -79,32 +84,36 @@ function CandidateInformation() {
                   {vice.map((viceCandidate) => (
                     <div className="tile is-parent" key={viceCandidate.id}>
                       {viceCandidate.last_name !== 'Vote' && (
-                        <article className="tile is-child box notification is-white">
-                          <h1 className="title is-5 has-text-dark">
-                            {viceCandidate.first_name} {viceCandidate.last_name}
-                          </h1>
-                          <div className="card-content">
-                            <p className="subtitle is-3 has-text-dark">
-                              {viceCandidate.roll_num}
-                            </p>
-                            <p className="title is-5 has-text-dark is-italic">
-                              "{viceCandidate.agenda}"
-                            </p>
-                            <p className="subtitle has-text-dark has-text-weight-semibold">
-                              {viceCandidate.batch}
-                            </p>
-                            <p className="subtitle has-text-dark has-text-weight-semibold">
-                              {viceCandidate.course}, {viceCandidate.department}
-                            </p>
+                        <div className="card">
+                          <div className="card-content has-text-centered">
+                            <div className="circle-image">
+                              <img
+                                className="is-rounded"
+                                src="public/images/O.jpeg"
+                                alt="Candidate Image"
+                              />
+                            </div>
+                            <div className="card-text">
+                              <h1 className="title is-5 has-text-dark">
+                                {viceCandidate.first_name}{' '}
+                                {viceCandidate.last_name}
+                              </h1>
+                              <p className="subtitle is-3 has-text-dark">
+                                {viceCandidate.roll_num}
+                              </p>
+                              <p className="title is-5 has-text-dark is-italic">
+                                "{viceCandidate.agenda}"
+                              </p>
+                              <p className="subtitle has-text-dark has-text-weight-semibold">
+                                {viceCandidate.batch}
+                              </p>
+                              <p className="subtitle has-text-dark has-text-weight-semibold">
+                                {viceCandidate.course},{' '}
+                                {viceCandidate.department}
+                              </p>
+                            </div>
                           </div>
-                          <figure className="image">
-                            <img
-                              className="is-rounded"
-                              src="public/images/OTIS.jpeg"
-                              alt="Candidate Image"
-                            />
-                          </figure>
-                        </article>
+                        </div>
                       )}
                     </div>
                   ))}
